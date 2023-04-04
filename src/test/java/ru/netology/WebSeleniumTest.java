760e1a42f06245ed06f58a6a516ae2316d55435a
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-04-04 21:30:39 +0400

    add files project

---
+++ src/test/java/ru/netology/WebSeleniumTest.java
@@ -0,0 +1,47 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WebSeleniumTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setDriver() {
+
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+    @AfterEach
+    void setDown() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldSiteCheckTest1() {
+        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Петров Петр");
+        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("+79123456789");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']input")).getText().trim();
+
+        assertEquals(expected,actual);
+    }
+
+}
